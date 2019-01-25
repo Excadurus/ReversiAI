5e3e9d2ffd343435c3e1c48443032ab9c41af602
Excadurus <Excadurus@gmail.com>
2019-01-25 11:20:04 +0330

    Final Code Master Push
    The Priority Values might change but the Code is Finished
    GG WP

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ReversiAI
 AI for the Game Reversi, KNTU Project
+Currently The AI works using a Priority(weight) System. This is the Priority
+        0, 7, 1, 4, 4, 1, 7, 0
+        7, 8, 6, 5, 5, 6, 8, 7
+        1, 6, 2, 3, 3, 2, 6, 1
+        4, 5, 3, 0, 0, 3, 5, 4
+        4, 5, 3, 0, 0, 3, 5, 4
+        1, 6, 2, 3, 3, 2, 6, 1
+        7, 8, 6, 5, 5, 6, 8, 7
+        0, 7, 1, 4, 4, 1, 7, 0
+Algorithmwise, The AI first finds all Available moves then chooses the move with the highest priority(Lowest Number)if there are multiple available moves with the same
+High score, then AI choosen the last One
+finally it show the best move by Printing the X and Y for that move (arraywise this is what x and y are : Array[Y][X]) 
+By Reza Liaghat and Ali Ghayoomi
--- otello.c
+++ otello.c
@@ -1,21 +1,11 @@
 #include<stdio.h>
-void movefinder(char board[][8][3]);
-void MoveChoose(char board[][8][3],int *x,int *y);
+void movefinder(char board[][8][3]);//peida kardan harekat mojaz
+void MoveChoose(char board[][8][3],int *x,int *y);//Entekhab Kardan Harekat Mojaz
 #define you '1'//khaneye siyah ke maeem =1
 #define rival '2'//khaneye sefid harif=2
 int main(int argc, char const *argv[])
 {
     char board[8][8][3]; //board bazi
-    /*char weightboard[8][8]={
-        {99,  -8,  8,  6,  6,  8,  -8, 99},
-        {-8, -24, -4, -3, -3, -4, -24, -8},
-        { 8,  -4,  7,  4,  4,  7,  -4,  8},
-        { 6,  -3,  4,  0,  0,  4,  -3,  6}, 
-        { 6,  -3,  4,  0,  0,  4,  -3,  6},
-        { 8,  -4,  7,  4,  4,  7,  -3,  8},
-        {-8, -24,  8,  6,  6,  8, -24, -8},
-        {99,  -8,  8,  6,  6,  8,  -8, 99}
-    }; */
     char weightboard[8][8]={
         {0, 7, 1, 4, 4, 1, 7, 0},
         {7, 8, 6, 5, 5, 6, 8, 7},
@@ -25,50 +15,37 @@ int main(int argc, char const *argv[])
         {1, 6, 2, 3, 3, 2, 6, 1},
         {7, 8, 6, 5, 5, 6, 8, 7},
         {0, 7, 1, 4, 4, 1, 7, 0}
-    };//Board's Weight, The lower the Value. The Better the move. Basically Each number is a Priority
+    };//Board's Weight, The lower the Value. The Better the move. Basically Each number is a Priority.
     for(int i=0;i<8;i++){
         for(int j=0;j<8;j++){
-            board[i][j][2]=weightboard[i][j];//Laye Wazn Bazi
+            board[i][j][2]=weightboard[i][j];//Laye Wazn Bazi ra be board asli enteghal midahim
             board[i][j][1]=argv[i+1][j];//roye bazi
             board[i][j][0]='0';//roye zirin baraye tayiin emkan harekat 1 baraye valid
         }
     }
     int x=0,y=0;//x , y Khorooji
     int *xptr=&x,*yptr=&y;//Pointer be X,Y baraye Function
-    movefinder(board);//peida kardan harekat mojaz
-    MoveChoose(board,xptr,yptr);//Entekhab Kardan Harekat Mojaz
-    printf("%d %d",x,y);
-    /*for(int i=0;i<8;i++){
-        for(int j=0;j<8;j++){
-            printf("%c",board[i][j][1]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    for(int i=0;i<8;i++){
-        for(int j=0;j<8;j++){
-
-            printf("%c",board[i][j][0]);
-        }
-        printf("\n");
-    }*/
+    movefinder(board);
+    MoveChoose(board,xptr,yptr);
+    printf("%d %d",x,y);//Khorooji tabe, Behtarin Harkat mojood ra ba tavajoh be vazn ha chap mikonad
     return 0;
 }
 void MoveChoose(char board[][8][3],int *x,int *y)
 {
-    int checkmax=9;
+    int checkmax=9;//in badtarin olaviat momken ast ke az hame olaviat haye mojood dar board ham bishtar ast. dar natije 100% meghdar an avaz mishavad
     for(int i=0;i<8;i++)
     {
         for(int j=0;j<8;j++)
         {
-            if(board[i][j][0]=='1' && board[i][j][2]<checkmax)
+            if(board[i][j][0]=='1' && board[i][j][2]<checkmax)//agar harkat mojaz bood va olaviat an behtar bood meghdar an jaygozin mishavad
             {
-                checkmax=board[i][j][2];
-                *x=j;
+                checkmax=board[i][j][2];//olaviat checkmax ra jadid tarin olaviat mikonad ta olaviat haye bad tar az halat konooni digar jaygozin nashavand
+                *x=j;// I va J ra Baraks be X,Y midahim zira satr va sotoon dar array barakse x va y dar nemoodar amal mikonand
                 *y=i;
             }
         }
     }
+    return;
 }
 void movefinder(char board[][8][3]){
     for(int i=0;i<8;i++){
